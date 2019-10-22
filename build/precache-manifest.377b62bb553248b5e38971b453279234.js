40262373249fc0d9f15081004fab5953bfc68364
curgery <androidgeek54@gmail.com>
2019-10-22 19:53:01 -0400

    initial commit

---
+++ build/precache-manifest.377b62bb553248b5e38971b453279234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a01fc21f2ae31366feaef695aff1e53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f160b0e85be605b198b0",
+    "url": "/static/css/main.b4fa477a.chunk.css"
+  },
+  {
+    "revision": "d62bafdf1385fc654852",
+    "url": "/static/js/2.20c58096.chunk.js"
+  },
+  {
+    "revision": "f160b0e85be605b198b0",
+    "url": "/static/js/main.3c0a0b14.chunk.js"
+  },
+  {
+    "revision": "4d717685ca8c9694cb24",
+    "url": "/static/js/runtime-main.72adbdcc.js"
+  },
+  {
+    "revision": "5b4fd1a3988435419c429bc025b6b252",
+    "url": "/static/media/rlg-cv.5b4fd1a3.jpg"
+  }
+]);
